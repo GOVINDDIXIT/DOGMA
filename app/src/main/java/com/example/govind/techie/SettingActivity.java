6039182aa6a6bca1e7268ab4d68d304671cf883e
Govind Dixit <lit2017018@iiita.ac.in>
2019-03-04 03:03:54 +0530

    fix: setting toolbar title

--- app/src/main/java/com/example/govind/techie/SettingActivity.java
+++ app/src/main/java/com/example/govind/techie/SettingActivity.java
@@ -6,16 +6,22 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatDelegate;
 import android.widget.CompoundButton;
 import android.widget.Switch;
+import android.widget.Toolbar;
 
 public class SettingActivity extends AppCompatActivity {
     private Switch mswitch;
+    private Toolbar toolbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_setting);
 
+        toolbar=findViewById(R.id.toolbar);
         mswitch = (Switch)findViewById(R.id.mswitch);
+
+        toolbar.setTitle("Settings");
+
         if(AppCompatDelegate.getDefaultNightMode()==AppCompatDelegate.MODE_NIGHT_YES) {
             mswitch.setChecked(true);
         }
